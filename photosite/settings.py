b7c3eb3b251c7b5f7fec3f749308414b9484c3a0
Alex <68821187+Draglex-cloud@users.noreply.github.com>
2020-08-11 23:08:41 +0300

    Update settings.py

--- photosite/settings.py
+++ photosite/settings.py
@@ -88,6 +88,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/static/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
