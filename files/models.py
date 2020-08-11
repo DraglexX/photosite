d56b038591b98cbb3c0fe29667da379cb71f1ba1
Alex <68821187+Draglex-cloud@users.noreply.github.com>
2020-08-12 01:16:57 +0300

    Update models.py

--- files/models.py
+++ files/models.py
@@ -1,21 +1,21 @@
-from django.db import models
+# from django.db import models
 
 
-class Album(models.Model):
-    id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
-    title = models.CharField(max_length=20)
-    description = models.CharField(max_length=150)
-    author = models.TextField(default=None)
+# class Album(models.Model):
+#     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+#     title = models.CharField(max_length=20)
+#     description = models.CharField(max_length=150)
+#     author = models.TextField(default=None)
 
-    class Meta:
-        ordering = ['title']
+#     class Meta:
+#         ordering = ['title']
 
 
-class Image(models.Model):
-    id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
-    title = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='images')
-    album_id = models.ForeignKey(Album, on_delete=models.CASCADE)
+# class Image(models.Model):
+#     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+#     title = models.CharField(max_length=20)
+#     image = models.ImageField(upload_to='images')
+#     album_id = models.ForeignKey(Album, on_delete=models.CASCADE)
 
-    class Meta:
-        ordering = ['title']
+#     class Meta:
+#         ordering = ['title']
