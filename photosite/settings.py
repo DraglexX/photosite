cb61ac76767af0f5c9c9760d099863d42c8b04de
Alex <68821187+Draglex-cloud@users.noreply.github.com>
2020-08-13 00:33:00 +0300

    Update settings.py

--- photosite/settings.py
+++ photosite/settings.py
@@ -16,7 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'files',
+    'photosite.files',
     'sorl.thumbnail',
 ]
 
