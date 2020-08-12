ec245c0ca0c45c754427abcf93acd111cb18202d
Alex <68821187+Draglex-cloud@users.noreply.github.com>
2020-08-13 00:19:43 +0300

    Update 0001_initial.py

--- files/migrations/0001_initial.py
+++ files/migrations/0001_initial.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=20)),
                 ('image', models.ImageField(upload_to='images')),
-                ('album_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='pmanage.Album')),
+                ('album_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='files.Album')),
             ],
             options={
                 'ordering': ['title'],
