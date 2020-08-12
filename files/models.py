f35c7eb7911f32ecd69154f65c1487c60290656b
Alex <68821187+Draglex-cloud@users.noreply.github.com>
2020-08-12 21:59:30 +0300

    Update models.py

--- files/models.py
+++ files/models.py
@@ -5,7 +5,8 @@ class Album(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=150)
-    author = models.CharField(max_length=150)
+#   author = models.CharField(max_length=150)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['title']
